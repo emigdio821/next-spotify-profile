03be24f532a3449cb1419f30dbff1ab1efc0caa0
Emigdio Torres <emigdio821@gmail.com>
2022-08-25 11:24:56 -0500

    feat: added eslint and initial config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,48 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable react/jsx-props-no-spreading */
+import {
+  ColorScheme,
+  MantineProvider,
+  ColorSchemeProvider,
+} from '@mantine/core'
+import Head from 'next/head'
+import { AppProps } from 'next/app'
+import { useHotkeys, useLocalStorage } from '@mantine/hooks'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'mantine-color-scheme',
+    defaultValue: 'dark',
+    getInitialValueInEffect: true,
+  })
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
+
+  useHotkeys([['mod+J', () => toggleColorScheme()]])
+
+  return (
+    <>
+      <Head>
+        <title>Spotify Profile</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <meta name="description" content="Your Spotify Profile" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{ colorScheme }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  )
 }
-
-export default MyApp
